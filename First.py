f3c014abfb0ec929e25e23aaa509edf2c7c5d1d0
Vishali <163227290+Vishali24m@users.noreply.github.com>
2026-04-16 08:58:35 +0400

    Add First.py to demonstrate quote usage in Python

---
+++ First.py
@@ -0,0 +1,18 @@
+# This is to demostrate the single quote, double quote and 3 single quote
+overlap="This is versatile in using quotes -by Vishali's Python Programming"
+print(overlap)
+Disp='''
+    *
+   ***
+  *****
+ *******
+   ||   
+
+   (O)   (O)
+       |
+      ---
+
+'''
+print(Disp)
+print(overlap)  # Can print no of times if we asigned in variable
+print(Disp)
